d83b9aa252c49f30d48dc738955a58ad4c67bfbd
Alexandre Mota <alexandremota.es@outlook.com>
2016-09-10 17:14:16 -0300

    icon, splash e joke activity

---
+++ app/src/main/java/com/alexandremota/easyjoke_android/ActivityJoke.java
@@ -0,0 +1,13 @@
+package com.alexandremota.easyjoke_android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityJoke extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_activity_joke);
+    }
+}
