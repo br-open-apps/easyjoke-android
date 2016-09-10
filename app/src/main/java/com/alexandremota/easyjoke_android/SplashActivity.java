07338773b706277a94935ea3f856061b3141a178
Alexandre Mota <alexandremota.es@outlook.com>
2016-09-10 15:05:06 -0300

    iniciando splash

---
+++ app/src/main/java/com/alexandremota/easyjoke_android/SplashActivity.java
@@ -0,0 +1,30 @@
+package com.alexandremota.easyjoke_android;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Timer().schedule(new TimerTask() {
+
+            @Override
+            public void run() {
+                finish();
+
+                Intent intent = new Intent();
+                intent.setClass(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        }, 2000);
+
+    }
+}
