e331f9c3b114a00f9b7d482f0649d6e5746b27e0
Phelipe Alves de Souza <phelipealvesdesouza@gmail.com>
2016-09-11 23:51:30 -0300

    Adicionando menu lateral

--- app/src/main/java/com/alexandremota/easyjoke_android/MainActivity.java
+++ app/src/main/java/com/alexandremota/easyjoke_android/MainActivity.java
@@ -2,6 +2,10 @@ package com.alexandremota.easyjoke_android;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.DrawerBuilder;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        Drawer result = new DrawerBuilder()
+                .withActivity(this)
+                .withToolbar(toolbar)
+                .build();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+        result.getActionBarDrawerToggle().setDrawerIndicatorEnabled(true);
     }
 }
